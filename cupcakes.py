5f2892d71b5500e007b71fff4088d6e64e49dd07
lanemunds <lanemunds@gmail.com>
2022-10-17 15:31:18 -0600

    second commit

--- cupcakes.py
+++ cupcakes.py
@@ -1,7 +1,12 @@
-class Cupcake:
-        size = "regular"
+from abc import ABC
+
+
+from abc import ABC,abstractmethod
+
+class Cupcake(ABC):
+    size = "regular"
     
-    def __init__(self,size,name,price,flavor,frosting,sprinkles,filling):
+    def __init__(self,name,price,flavor,frosting,sprinkles,filling):
         self.name = name
         self.price = price
         self.flavor = flavor
@@ -11,11 +16,27 @@ class Cupcake:
     def add_sprinkles(self,*args):
         for sprinkle in args:
             self.sprinkles.append(sprinkle)
+    @abstractmethod        
+    def calculate_price(self,quantity):
+        return quantity * self.price
         
-            
+
+class Mini(Cupcake):
+    size = "mini" 
+    def __init__(self,name,price,flavor,frosting,sprinkles):
+        self.name = name
+        self.price = price
+        self.flavor = flavor
+        self.frosting = frosting
+        self.sprinkles = sprinkles
+        
+    
+    
+    
+my_cupcake_mini = Mini("white",2.99,"white","raspberry",["white chocolate"])          
     
 
-my_cupcake = Cupcake("large","red velvet",4.99,"red velvet","cream cheese",[],True)      
+my_cupcake = Cupcake("red velvet",4.99,"red velvet","cream cheese",[],True)      
 
 my_cupcake.frosting = "chocolate"
 my_cupcake.filling = "chocolate"
@@ -23,4 +44,5 @@ my_cupcake.name = "double chocolate red velvet"
 my_cupcake.is_miniature = False
 
 my_cupcake.add_sprinkles("chocolate","rainbow")
-print(my_cupcake.sprinkles)
+
+
