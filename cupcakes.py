18d6bd09347619573eedbdbb74c4fbc158ac1bd2
lanemunds <lanemunds@gmail.com>
2022-10-18 15:03:07 -0600

    part3 step 1

--- cupcakes.py
+++ cupcakes.py
@@ -1,7 +1,6 @@
-from abc import ABC
-
-
 from abc import ABC,abstractmethod
+import csv
+from pprint import pprint
 
 class Cupcake(ABC):
     size = "regular"
@@ -29,20 +28,54 @@ class Mini(Cupcake):
         self.flavor = flavor
         self.frosting = frosting
         self.sprinkles = sprinkles
-        
     
+    def calculate_price(self,quantity):
+        return quantity * self.price    
+     
+     
     
+class large(Cupcake):
+    size = "large" 
+    def __init__(self,name,price,flavor,frosting,sprinkles):
+        self.name = name
+        self.price = price
+        self.flavor = flavor
+        self.frosting = frosting
+        self.sprinkles = sprinkles
     
+    def calculate_price(self,quantity):
+        return quantity * self.price    
+     
+class regular(Cupcake):
+    size = "regular" 
+    def __init__(self,name,price,flavor,frosting,sprinkles):
+        self.name = name
+        self.price = price
+        self.flavor = flavor
+        self.frosting = frosting
+        self.sprinkles = sprinkles
+    
+    def calculate_price(self,quantity):
+        return quantity * self.price    
+    
+def read_csv(file)   
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile) 
+        for row in reader:
+            pprint(row)
+
+read_csv("starter/sample.csv")
+
 my_cupcake_mini = Mini("white",2.99,"white","raspberry",["white chocolate"])          
     
 
-my_cupcake = Cupcake("red velvet",4.99,"red velvet","cream cheese",[],True)      
+# my_cupcake = Cupcake("red velvet",4.99,"red velvet","cream cheese",[],True)      
 
-my_cupcake.frosting = "chocolate"
-my_cupcake.filling = "chocolate"
-my_cupcake.name = "double chocolate red velvet"
-my_cupcake.is_miniature = False
+# my_cupcake.frosting = "chocolate"
+# my_cupcake.filling = "chocolate"
+# my_cupcake.name = "double chocolate red velvet"
+# my_cupcake.is_miniature = False
 
-my_cupcake.add_sprinkles("chocolate","rainbow")
+# my_cupcake.add_sprinkles("chocolate","rainbow")
 
 
