57e42c9c5ac80975b5ecd19b1cbb7ba50563c45d
lanemunds <lanemunds@gmail.com>
2022-10-18 16:03:36 -0600

    finished part 3

--- cupcakes.py
+++ cupcakes.py
@@ -22,51 +22,106 @@ class Cupcake(ABC):
 
 class Mini(Cupcake):
     size = "mini" 
-    def __init__(self,name,price,flavor,frosting,sprinkles):
+    def __init__(self,name,price,flavor,frosting):
         self.name = name
         self.price = price
         self.flavor = flavor
         self.frosting = frosting
-        self.sprinkles = sprinkles
+        self.sprinkles = []
     
     def calculate_price(self,quantity):
         return quantity * self.price    
      
      
     
-class large(Cupcake):
+class Large(Cupcake):
     size = "large" 
-    def __init__(self,name,price,flavor,frosting,sprinkles):
+    def __init__(self,name,price,flavor,frosting):
         self.name = name
         self.price = price
         self.flavor = flavor
         self.frosting = frosting
-        self.sprinkles = sprinkles
+        self.sprinkles = []
     
     def calculate_price(self,quantity):
         return quantity * self.price    
      
-class regular(Cupcake):
+class Regular(Cupcake):
     size = "regular" 
-    def __init__(self,name,price,flavor,frosting,sprinkles):
+    def __init__(self,name,price,flavor,frosting):
         self.name = name
         self.price = price
         self.flavor = flavor
         self.frosting = frosting
-        self.sprinkles = sprinkles
+        self.sprinkles = []
     
     def calculate_price(self,quantity):
         return quantity * self.price    
     
-def read_csv(file)   
+def read_csv(file):   
     with open(file) as csvfile:
         reader = csv.DictReader(csvfile) 
         for row in reader:
             pprint(row)
 
-read_csv("starter/sample.csv")
+def write_new_csv(file, cupcakes):
+    with open(file,"w",newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames= fieldnames)
+        
+        writer.writeheader()
+        
+        for cupcake in cupcakes:
+            if hasattr(cupcake, "filling"):
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
+            else:
+                writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
 
-my_cupcake_mini = Mini("white",2.99,"white","raspberry",["white chocolate"])          
+
+
+def add_cupcake(file, cupcake):
+    with open(file, "a", newline="\n") as csvfile:
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if hasattr(cupcake, "filling"):
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "filling": cupcake.filling, "sprinkles": cupcake.sprinkles})
+        else:
+            writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
+
+
+
+
+cupcake4 = Mini("banana cream",.99,"banana","cream")
+
+
+
+add_cupcake("newCupcakeList.csv", cupcake4)
+    
+
+
+
+cupcake1 = Regular("Stars and Stripes", 2.99, "Vanilla", "Vanilla")
+cupcake1.add_sprinkles("Red", "White", "Blue")
+cupcake2 = Mini("Oreo", .99, "Chocolate", "Cookies and Cream")
+cupcake2.add_sprinkles("Oreo pieces")
+cupcake3 = Large("Red Velvet", 3.99, "Red Velvet", "Cream Cheese")
+
+cupcake_list = [
+    cupcake1,
+    cupcake2,
+    cupcake3
+]       
+        
+
+
+
+
+
+# write_new_csv("newCupcakeList.csv", cupcake_list)
+# read_csv("newCupcakeList.csv")
+
+# my_cupcake_mini = Mini("white",2.99,"white","raspberry",["white chocolate"])          
     
 
 # my_cupcake = Cupcake("red velvet",4.99,"red velvet","cream cheese",[],True)      
