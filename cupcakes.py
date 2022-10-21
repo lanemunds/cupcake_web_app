095c969b61415f1515ac90ce21c27c94f8c45d1d
lanemunds <lanemunds@gmail.com>
2022-10-21 12:29:26 -0600

    part 5

--- cupcakes.py
+++ cupcakes.py
@@ -90,13 +90,33 @@ def add_cupcake(file, cupcake):
             writer.writerow({"size": cupcake.size, "name": cupcake.name, "price": cupcake.price, "flavor": cupcake.flavor, "frosting": cupcake.frosting, "sprinkles": cupcake.sprinkles})
 
 
+def get_cupcakes(file):
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        reader = list(reader)
+        return reader
+    
+    
+def find_cupcake(file, name):
+    for cupcake in get_cupcakes(file):
+        if cupcake ["name"]==name:
+            return cupcake
+    return None        
+    
+def add_cupcake_dictionary(file,cupcake):
+    with open(file,"a",newline="\n") as csvfile:
+        fieldnames = ["size","name","price","flavor","frosting","sprinkles","filling"]
+        writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        writer.writerow(cupcake)    
+    
+    
+  
+    
+
+cupcake4 = Mini("Banana_cream", 3.99, "Banana", "Cream")
 
 
-cupcake4 = Mini("banana cream",.99,"banana","cream")
 
-
-
-add_cupcake("newCupcakeList.csv", cupcake4)
     
 
 
@@ -113,6 +133,7 @@ cupcake_list = [
     cupcake3
 ]       
         
+add_cupcake("order.csv", cupcake4)
 
 
 
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from unicodedata import name
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
 
 
 
@@ -9,17 +11,27 @@ def home():
 
 @app.route("/cupcakes")
 def all_cupcakes():
-    return render_template("cupcakes.html")
+    return render_template("cupcakes.html", cupcakes = get_cupcakes("newCupcakeList.csv"))
 
-@app.route("/cupcake_individual")
-def individual_cupcake():
-    return render_template("individual-cupcakes.html")
+@app.route("/cupcake_individual/<name>")
+def individual_cupcake(name):
+    return render_template("individual-cupcakes.html", cupcake= find_cupcake("newCupcakeList.csv", name))
 
 @app.route("/order")
 def order():
-    return render_template("order.html")
+    return render_template("order.html",cupcakes = get_cupcakes("order.csv"))
 
+@app.route("/add-cupcake/<name>")
+def add_cupcake(name):
+    cupcake = find_cupcake("newCupcakeList.csv", name)
+
+    if cupcake:
+        add_cupcake_dictionary("order.csv", cupcake)
+        return redirect(url_for("all_cupcakes"))
+    else:
+        return "Sorry cupcake not found."    
 
 if __name__ == "__main__":
     app.env = "development"
     app.run(debug = True, port = 8000, host = "localhost")
+    
